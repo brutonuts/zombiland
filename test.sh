0fed5188ae729c2bcdeb60880c1b9d404b16ce92
brutonuts <119691622+brutonuts@users.noreply.github.com>
2022-12-03 14:44:18 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # edit port 
-sed '/^#Port/s/22/1488/' -i /etc/ssh/nano sshd_config
+sed '/^#Port/s/22/1488/' -i /etc/ssh/sshd_config
 # edit port-#
-sed -i 's/#Port/Port/g' -i /etc/ssh/nano sshd_config
+sed -i 's/#Port/Port/g' -i /etc/ssh/sshd_config
 # edit root+#
-sed -i 's/PermitRootLogin/#PermitRootLogin/g' -i /etc/ssh/nano sshd_config
+sed -i 's/PermitRootLogin/#PermitRootLogin/g' -i /etc/ssh/sshd_config
 # edit root
-sed '/^#PermitRootLogin/s/yes/no/' -i /etc/ssh/nano sshd_config
+sed '/^#PermitRootLogin/s/yes/no/' -i /etc/ssh/sshd_config
 
 apt update
 apt install curl -y
