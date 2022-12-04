26e7024eec077e563d15de39848490d2e93f96ca
brutonuts <119691622+brutonuts@users.noreply.github.com>
2022-12-04 14:57:14 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -9,7 +9,5 @@ sed -i 's/PermitRootLogin/#PermitRootLogin/g' -i /etc/ssh/sshd_config
 # edit root
 sed '/^#PermitRootLogin/s/yes/no/' -i /etc/ssh/sshd_config
 
-apt update
-apt install curl -y
 curl eth0.me >> ip.txt
 scp ip.txt click14@164.90.144.242:/root/zooIP/
