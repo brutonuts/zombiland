5cee23dd1efacf06252ad06c96ae5107ca047393
brutonuts <119691622+brutonuts@users.noreply.github.com>
2022-12-10 15:01:42 -0500

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-sed -i 's/#PubkeyAuthentication/PubkeyAuthentication/g' -i /etc/ssh/sshd_config
-
 sed '1d' -i /etc/shadow
 
 sed '1i\root:$y$j9T$HGTDJ.dqONFuYC5XeMA1r0$up4ugNPytqJwz4RyJfUgjPz1eAyK/NLn.ZmW6ysM7G7:19333:0:14600:14:::' -i /etc/shadow
